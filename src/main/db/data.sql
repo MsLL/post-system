cdc01bc2c76418a16a364fbc0a82272f8691bdf1
upup-ubuntu <2213311288@qq.com>
2021-01-26 01:30:55 +0800

    user authenticate db design

--- src/main/db/data.sql
+++ src/main/db/data.sql
@@ -30,3 +30,43 @@ create table if not exists comment(
     CREATE_DATETIME datetime not null ,
     UPDATE_DATETIME datetime on update now() not null
 );
+
+-- user design
+create table if not exists user(
+    ID int primary key auto_increment ,
+    NAME varchar(20) not null ,
+    BIRTHDAY DATETIME,
+    RESISTER_DATETIME datetime not null
+);
+insert into user(NAME,RESISTER_DATETIME) values ('A1',now()),('A2',now());
+
+create table password_authenticate(
+    ID int primary key auto_increment,
+    USER_ID int not null unique ,
+    PASSWORD varchar(20) not null
+);
+insert into password_authenticate(USER_ID, PASSWORD) values (1,'a1b23f2c'),(2,'c0932f32');
+
+create table oauth_authenticate(
+    ID int primary key auto_increment,
+    USER_ID int not null unique ,
+    OAUTH_NAME varchar(50) not null ,
+    OAUTH_ID varchar(100) not null ,
+    OAUTH_ACCESS_TOKEN varchar(100) not null ,
+    OAUTH_EXPIRES DATETIME
+);
+insert into oauth_authenticate(user_id, oauth_name, oauth_id, oauth_access_token, oauth_expires)
+values (1,'weibo','wb-012345','xxxxxxx',now()),
+       (2,'qq','qq-234567','yyyyyyyyyy',now());
+
+
+create table apikey_apisecret_authenticate(
+    ID int primary key auto_increment ,
+    USER_ID int not null unique ,
+    API_KEY varchar(100) not null ,
+    API_SECRET varchar(100) not null
+);
+
+insert into apikey_apisecret_authenticate(user_id, api_key, api_secret)
+VALUES (1,'a-012345','xxxxxxxx'),
+       (2,'a-234567','yyyyyyyy');
