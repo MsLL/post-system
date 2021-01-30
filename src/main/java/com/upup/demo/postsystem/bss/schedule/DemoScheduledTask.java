44599e2cecabe3419f2e549f6800d1cad9ba9694
upup-ubuntu <2213311288@qq.com>
2021-01-30 22:09:56 +0800

    添加spring自己的定时任务demo；不耗费资源的，调度频率比较低的job可以直接用它，在应用内部跑。否则应该用类似xxljob之类的分布式任务调度框架。

---
+++ src/main/java/com/upup/demo/postsystem/bss/schedule/DemoScheduledTask.java
@@ -0,0 +1,23 @@
+package com.upup.demo.postsystem.bss.schedule;
+
+import java.time.LocalDateTime;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author tao.li
+ * @Date 2021/1/30 下午9:56
+ * see https://juejin.cn/post/6844903968292732941
+ * @EnableScheduling注解 声明在ScheduleConfigure类上，没有直接标注在PostSystemApplication类上。那样不优雅。配置统一放在config目录下。
+ */
+
+
+@Component
+public class DemoScheduledTask {
+
+    @Scheduled(fixedRate = 3000)
+    public void scheduledTask() {
+        System.out.println("      Demo Scheduled 任务执行时间：" + LocalDateTime.now());
+    }
+
+}
---
+++ src/main/java/com/upup/demo/postsystem/config/ScheduleConfigure.java
@@ -0,0 +1,16 @@
+package com.upup.demo.postsystem.config;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author tao.li
+ * @Date 2021/1/30 下午10:00
+ * 一个空Component,只是为了声明@EnableScheduling注解。
+ * 没有直接标注在PostSystemApplication类上。那样不优雅。配置统一放在config包下。
+ */
+
+@EnableScheduling
+@Component
+public class ScheduleConfigure {
+}
