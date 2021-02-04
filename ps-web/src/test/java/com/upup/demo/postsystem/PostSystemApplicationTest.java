c7e64f5bec3095b5a60425fd6ef6ebcee0075fa4
upup-ubuntu <2213311288@qq.com>
2021-02-04 23:01:05 +0800

    dev打印mybatis的log到标准输出

--- ps-web/src/test/java/com/upup/demo/postsystem/PostSystemApplicationTest.java
+++ ps-web/src/test/java/com/upup/demo/postsystem/PostSystemApplicationTest.java
@@ -1,7 +1,12 @@
 package com.upup.demo.postsystem;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,13 +14,22 @@ import static org.junit.jupiter.api.Assertions.*;
  * @Author tao.li
  * @Date 2021/1/25 下午8:47
  */
+
+@SpringBootTest
 class PostSystemApplicationTest {
 
-    @BeforeEach
-    void setUp() {
+    @Autowired
+    Environment environment;
+
+    @Test
+    public void test0(){
+        System.out.println(Arrays.toString(environment.getDefaultProfiles()));
+        System.out.println(Arrays.toString(environment.getActiveProfiles()));
+
+        System.out.println(environment.getProperty("upup.xxx"));
     }
 
-    @AfterEach
-    void tearDown() {
+    public void test1(){
+
     }
 }
