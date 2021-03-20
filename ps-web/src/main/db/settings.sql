4d67c71583acf68b9bb2bd1b541129d9ace254f7
upup-ubuntu <2213311288@qq.com>
2021-03-20 13:56:44 +0800

    mysql dev max_connections 调整

--- ps-web/src/main/db/settings.sql
+++ ps-web/src/main/db/settings.sql
@@ -4,3 +4,10 @@ show variables like '%time_zone%';
 set global time_zone = '+8:00';  ##修改mysql全局时区为北京时间，即我们所在的东8区
 set time_zone = '+8:00';  ##修改当前会话时区
 flush privileges;  #立即生效
+
+
+-- change max_connections,default value is 151
+-- todo 继续调参，mysqlslap并发4k mysql就直接挂了
+show variables like '%connections%';
+set global max_connections=10000;
+flush privileges ;
