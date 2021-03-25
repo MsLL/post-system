3800fd8d659bf39d88c4e8af99537bc20a155fdd
upup-ubuntu <2213311288@qq.com>
2021-03-25 20:28:11 +0800

    save

--- ps-web/src/main/java/com/upup/demo/postsystem/config/SocketIOAutoConfiguration.java
+++ ps-web/src/main/java/com/upup/demo/postsystem/config/SocketIOAutoConfiguration.java
@@ -13,13 +13,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 
-/**
- * NettySocketConfig
- *
- * @author XiongNeng
- * @version 1.0
- * @since 2018/1/19
- */
 @Configuration
 @EnableConfigurationProperties(SocketIOProperties.class)
 public class SocketIOAutoConfiguration {
